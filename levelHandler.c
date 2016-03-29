cec64e7dff0f6775548a6c38d01bb6144c59420e
silvermoon <raphdruon@gmail.com>
2016-03-29 10:13:32 +0200

    Add level loader

---
+++ levelHandler.c
@@ -0,0 +1,89 @@
+
+#include <ncurses.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "levelHandler.h"
+#include <game.h>
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+game parse_level(char *levelFound)
+{
+    FILE *f = fopen(levelFound, "r");
+    if (!f) {
+        return NULL;
+    }
+    char buff[1024];
+    piece pieces[1024];
+    int i = 0;
+    if(!fgets(buff, 1023, f))
+        return NULL;
+    int width = buff[0] - '0';
+    int height = buff[2] - '0';
+    if(!fgets(buff, 1023, f))
+        return NULL;
+    int n = atoi(buff);
+    while (i < n) {
+        if(!fgets(buff, 1023, f))
+            return NULL;
+        int x, y, widthp, heightp, move_x, move_y;
+        x = buff[0] - '0';
+        y = buff[2] - '0';
+        widthp = buff[4] - '0';
+        heightp = buff[6] - '0';
+        move_x = buff[8] - '0';
+        move_y = buff[10] - '0';
+        if (x < 0 || x > 9 || y < 0 || y > 9 || move_x < 0 || move_x > 1 || move_y < 0 || move_y > 1 || widthp <= 0 || heightp <= 0) {
+            return NULL;
+        }
+        pieces[i] = new_piece(x, y, widthp, heightp, move_x, move_y);
+        i++;
+    }
+    game newGame = new_game(width, height, i, pieces);
+    return newGame;
+}
+
+char** find_levels(char *path, int *nblevel)
+{
+    char **arrFile, **tmp;
+    int i = 0, taille = 4;
+    arrFile = malloc(sizeof (char*)*taille);
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(path);
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if (i > taille) {
+                taille *= 2;
+                tmp = malloc(sizeof (char*)*taille);
+                for (int j = 0; j < i; j++)
+                    tmp[j] = arrFile[j];
+                free(arrFile);
+                arrFile = tmp;
+            }
+            //Get extension name
+            char *ext = strrchr(dir->d_name, '.');
+            if (!ext) {
+                /* no extension */
+            } else {
+                //Get only *.lvl file
+                if (strcmp(ext + 1, "lvl") == 0) {
+                    arrFile[i] = malloc(sizeof (char)*strlen(dir->d_name) + 1);
+                    strcpy(arrFile[i], dir->d_name);
+                    i++;
+                }
+            }
+        }
+        closedir(d);
+    }
+    *nblevel = i;
+    return arrFile;
+
+}
+
--- main.c
+++ main.c
@@ -16,44 +16,60 @@
 #include <time.h>
 #include "game.h"
 #include "solv.h"
+#include "levelHandler.h"
 
 /*
  * 
  */
 int main(int argc, char** argv)
 {
+    bool defaut = false;
     //Ane rouge
-    if (argc < 2) {
-        fprintf(stderr, "%s : NumberOfLoop\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "%s : NumberOfLoop Game Path_to_lvl\n Game : \t1 Rush Hour\n\t\t0 Ane Rouge\n", argv[0]);
         return EXIT_FAILURE;
     }
     int nbLoop = atoi(argv[1]);
     nbLoop = abs(nbLoop);
     if (nbLoop == 0) {
-        fprintf(stderr, "%s : NUMBEROfLoop\n", argv[0]);
+        fprintf(stderr, "%s : NUMBEROfLoop Game Path_to_lvl\n Game : \t1 Rush Hour\n\t\t0 Ane Rouge\n", argv[0]);
         return EXIT_FAILURE;
     }
+    int gameType = atoi(argv[2]);
+    gameType = abs(gameType);
+    if (gameType > 1) {
+        fprintf(stderr, "%s : NUMBEROfLoop Game Path_to_lvl\n Game : \t1 Rush Hour\n\t\t0 Ane Rouge\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    game newGame = NULL;
     piece pieces[10];
-    game newGame;
-    pieces[0] = new_piece(1, 3, 2, 2, true, true); //Rouge
-    pieces[1] = new_piece(3, 3, 1, 2, true, true); // 2
-    pieces[2] = new_piece(3, 1, 1, 2, true, true); // 3
-    pieces[3] = new_piece(3, 0, 1, 1, true, true); // 4
-    pieces[4] = new_piece(1, 2, 2, 1, true, true); // 5
-    pieces[5] = new_piece(2, 1, 1, 1, true, true); // 6
-    pieces[6] = new_piece(1, 1, 1, 1, true, true); // 7
-    pieces[7] = new_piece(0, 0, 1, 1, true, true); // 8
-    pieces[8] = new_piece(0, 1, 1, 2, true, true); // 9
-    pieces[9] = new_piece(0, 3, 1, 2, true, true); // 10
-    newGame = new_game(4, 5, 10, pieces);
+    if(argc == 4) {
+        printf("Load level\n");
+        newGame = parse_level(argv[3]);
+    }
+    if(!newGame) {
+        defaut=true;
+        printf("Load level failed\n");
+        pieces[0] = new_piece(1, 3, 2, 2, true, true); //Rouge
+        pieces[1] = new_piece(3, 3, 1, 2, true, true); // 2
+        pieces[2] = new_piece(3, 1, 1, 2, true, true); // 3
+        pieces[3] = new_piece(3, 0, 1, 1, true, true); // 4
+        pieces[4] = new_piece(1, 2, 2, 1, true, true); // 5
+        pieces[5] = new_piece(2, 1, 1, 1, true, true); // 6
+        pieces[6] = new_piece(1, 1, 1, 1, true, true); // 7
+        pieces[7] = new_piece(0, 0, 1, 1, true, true); // 8
+        pieces[8] = new_piece(0, 1, 1, 2, true, true); // 9
+        pieces[9] = new_piece(0, 3, 1, 2, true, true); // 10
+        newGame = new_game(4, 5, 10, pieces);
+    }
     gameStruct *result=NULL;
     
     
     for (int i = 0; i < nbLoop; i++) {
-        result = solv(newGame,ANE);
+        result = solv(newGame,gameType);
         if(!result){
-            printf("Impossible to found a solution !");
-            break;
+            printf("Impossible to found a solution !\n");
+            return EXIT_FAILURE;
         }
 	printf("Found in %d moves\n",game_nb_moves(result->current));
 #ifdef SHOWPATH
@@ -63,9 +79,10 @@ int main(int argc, char** argv)
         free(result);
     }
     
-    
-    for(int i=0;i<game_nb_pieces(newGame);i++)
-        delete_piece(pieces[i]);
+    if(defaut) {
+        for(int i=0;i<game_nb_pieces(newGame);i++)
+            delete_piece(pieces[i]);
+    }
     delete_game(newGame);
     return (EXIT_SUCCESS);
 }
